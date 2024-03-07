11190d767fc8fe39d6b4464ba584d8de59ee87cc
gilberthappi <gdushimimana6@gmail.com>
2024-03-07 15:38:52 +0200

    Update home from desktop -v4

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,15 +5,15 @@ const Home = () => (
       A math magician is an entertainer who performs mathematical tricks and illusions,
       often involving mental calculations and mathematical principles.
       Using quick calculations,
-      sleight of hand, and a deep understanding of mathematics, 
-      they create the illusion of performing seemingly impossible feats with numbers, 
+      sleight of hand, and a deep understanding of mathematics,
+      they create the illusion of performing seemingly impossible feats with numbers,
       captivating audiences with their mathematical prowess and skill.
       Math magicians may perform in various settings,
       from stage shows to educational events,
       showcasing the fun and intrigue of mathematics through engaging performances.
     </p>
     <p>
-      Mathematics as Poetry: Albert Einstein once remarked that mathematics is akin to the 
+      Mathematics as Poetry: Albert Einstein once remarked that mathematics is akin to the
       poetry of logical ideas. In this analogy, he beautifully portrays how mathematical concepts,
       when woven together, create a narrative that is as intricate and elegant as poetry.
 
