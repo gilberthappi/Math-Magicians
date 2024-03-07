4e4381e3ecd1468d739e8cb9f83689a1dfc7e698
gilberthappi <gdushimimana6@gmail.com>
2024-03-07 15:43:36 +0200

    Update home from desktop -v5

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,28 +3,20 @@ const Home = () => (
     <h1>Welcome to our Page!</h1>
     <p>
       A math magician is an entertainer who performs mathematical tricks and illusions,
-      often involving mental calculations and mathematical principles.
-      Using quick calculations,
-      sleight of hand, and a deep understanding of mathematics,
-      they create the illusion of performing seemingly impossible feats with numbers,
-      captivating audiences with their mathematical prowess and skill.
-      Math magicians may perform in various settings,
-      from stage shows to educational events,
-      showcasing the fun and intrigue of mathematics through engaging performances.
+      often involving mental calculations and principles. They use quick calculations,
+      sleight of hand, and a deep understanding of math to create seemingly impossible feats,
+      captivating audiences with their skill. Math magicians perform in various settings,
+      showcasing the fun of math through engaging performances.
     </p>
     <p>
-      Mathematics as Poetry: Albert Einstein once remarked that mathematics is akin to the
-      poetry of logical ideas. In this analogy, he beautifully portrays how mathematical concepts,
-      when woven together, create a narrative that is as intricate and elegant as poetry.
+      Mathematics as Poetry: Albert Einstein likened math to the poetry of logical ideas,
+      portraying how concepts weave together to form elegant narratives.
 
-      Mathematics as the Language of the Universe: Galileo Galilei suggested that mathematics
-      serves as the universal language written by the divine hand.
-      This perspective implies that through mathematical principles,
-      we gain insights into the underlying order and structure of the cosmos.
+      Mathematics as the Language of the Universe: Galileo Galilei viewed math as
+      the universal language written by the divine, revealing insights into cosmic order.
 
-      Pure Mathematics as Intellectual Artistry: Einstein further emphasized the aesthetic beauty found
-      in pure mathematics, likening it to the poetry of logical ideas. Here, mathematics transcends
-      mere calculation, becoming an art form where abstract concepts and logical reasoning intertwine.
+      Pure Mathematics as Intellectual Artistry: Einstein saw pure math as akin to poetry,
+      transcending mere calculation to become an art form of abstract concepts and reasoning.
     </p>
   </section>
 );
