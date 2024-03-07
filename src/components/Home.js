8e0d025b865d51aea904e6e27ac265b11ecb543e
gilberthappi <gdushimimana6@gmail.com>
2024-03-07 15:06:52 +0200

    Update home from desktop

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,13 @@ const Home = () => (
     <h1>Welcome to our Page!</h1>
     <p>
       A math magician is an entertainer who performs mathematical tricks and illusions,
-      often involving mental calculations and mathematical principles. Using quick calculations,
-      sleight of hand, and a deep understanding of mathematics, they create the illusion of performing seemingly impossible feats with numbers, 
+      often involving mental calculations and mathematical principles.
+      Using quick calculations,
+      sleight of hand, and a deep understanding of mathematics, 
+      they create the illusion of performing seemingly impossible feats with numbers, 
       captivating audiences with their mathematical prowess and skill.
-      Math magicians may perform in various settings, from stage shows to educational events,
+      Math magicians may perform in various settings,
+      from stage shows to educational events,
       showcasing the fun and intrigue of mathematics through engaging performances.
     </p>
     <p>
@@ -16,7 +19,8 @@ const Home = () => (
 
       "Pure mathematics is, in its way, the poetry of logical ideas." - Albert Einstein
 
-      "Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding." - William Paul Thurston
+      "Mathematics is not about numbers, equations, computations,
+       or algorithms: it is about understanding." - William Paul Thurston
 
       "Mathematics is the music of reason." - James Joseph Sylvester
     </p>
